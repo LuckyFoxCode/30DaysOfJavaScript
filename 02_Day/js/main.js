a164b6eb25f7d998334e8c68f4eb84d61145c563
Serhii Shabala <sergei181989@gmail.com>
2023-06-19 17:23:14 +0300

    :sparkles: feat: task day02

---
+++ 02_Day/js/main.js
@@ -0,0 +1,36 @@
+// Task 1.
+// let challenge = '30 Days Of JavaScript';
+
+// console.log(challenge);
+// console.log(challenge.length);
+// console.log(challenge.toUpperCase());
+// console.log(challenge.toLowerCase());
+// console.log(challenge.substring(3, 7));
+// console.log(challenge.slice(3));
+// console.log(challenge.includes('Script'));
+// console.log(challenge.split());
+// console.log(challenge.split(' '));
+// console.log('Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'.split(','));
+// console.log(challenge.replace('JavaScript', 'Python'));
+// console.log(challenge.charAt(15)); // S
+// console.log(challenge.charCodeAt('J')); // 51
+// console.log(challenge.indexOf('30 Days Of JavaScript'));
+// console.log(challenge.lastIndexOf('30 Days Of JavaScript'));
+// console.log(challenge.match('a'));
+
+// Task 2.
+// console.log(
+//   `The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.`,
+// );
+// console.log(
+//   `"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."`,
+// );
+// console.log(parseFloat('9.8'));
+// console.log('Питон'.includes('он'));
+// console.log('жаргон'.includes('он'));
+// console.log('Я надеюсь, что этот курс не полон жаргона'.includes('жаргон'));
+// console.log(Math.ceil(Math.random() * 100));
+// console.log(Math.round(50 + Math.random() * (100 - 50)));
+// console.log(Math.round(Math.random() * 255));
+// const str = 'JavaScript';
+// console.log(str[Math.round(Math.random() * (str.length - 1))]);

738f6811e5e06b64289216043b8ad3ecb4d015ea
Serhii Shabala <sergei181989@gmail.com>
2023-06-19 12:52:51 +0300

    :sparkles: feat: task day01

---
+++ 01_Day/js/datatypes.js
@@ -0,0 +1,13 @@
+console.log(typeof '');
+console.log(typeof 13);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+
+// let firstName, lastName, isMaried, country, age;
+
+let myAge = 25;
+let youAge = 20;
+
+console.log(`I'm ${myAge} years old`);
+console.log(`You are ${youAge} years old`);
---
+++ 01_Day/js/variable.js
@@ -0,0 +1,10 @@
+// comments can make code readable
+// welcome to 30DaysOfJavaScript
+/* 
+  comments can make code readable, easy to use and informative
+*/
+
+const userName = 'John';
+const isMeried = false;
+const age = undefined;
+const color = null;
